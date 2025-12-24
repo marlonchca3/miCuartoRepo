5dd3155d1a9490823426e9bd8eea9ced04aa9bfb
marlonchca3 <marlonchca3@gmail.com>
2025-12-24 00:21:57 -0500

    metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -15,6 +15,11 @@ def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
 
+@app.get("/cursosPlatzi")
+def get_cursos():    
+rows = ["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"]    
+return rows
+
 @app.get("/superheroesMarvel")
 def get_superheroes_marvel():
     rows = ["Spider-Man", "Iron Man", "Thor", "Hulk", "Black Widow", "Doctor Strange", "Black Panther", "Captain America"]
